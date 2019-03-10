abc1cb4db7b3fde9ed3bdc3f416f6104c12bb46a
A <ashton@As-MacBook-Air.local>
2019-03-10 16:55:12 -0400

    first lesson

---
+++ Lesson1/lesson.java
@@ -0,0 +1,21 @@
+/* Data types:
+int - 32 bits
+double - decimals
+boolean - 1 bit: true or false
+String - objects
+float -
+char -
+long -
+
+objects in java have a capital first letter.
+lowercase first letter data types are primitive.
+*/
+
+public class lesson {
+      public static void main(String args[]){
+          for (int a=1; a<102; a+=3) {
+              System.out.println(a);
+          }
+
+      }
+}
