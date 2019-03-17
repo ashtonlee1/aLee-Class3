8e91e0509ac187620d9b2719123f037c23fb61f5
ashtonlee1 <alee2@collegiateschool.org>
2019-03-17 19:24:02 -0400

    hw

--- Lesson1/hw.java
+++ Lesson1/hw.java
@@ -11,40 +11,41 @@ public class hw
     System.out.println(sum);
 
     //Problem 2
-    for(int b = 4; b<97; b+=10) {
-      int a = b + 2;
-      System.out.println(b);
-      System.out.println(a);
+    for(int a = 1; a<100; a++) {
+      if(a%5==1) {
+        System.out.println(a);
+      }
+      else if(a%5==4) {
+        System.out.println(a);
+      }
     }
 
     //Problem 3
+
     int score = 91;
-    if(score<81) {
-      if(score<71) {
-        System.out.println("D");
-      }
-      else {
-        System.out.println("C");
-      }
+    if(score<71) {
+      System.out.println("D");
     }
-    else {
-      if(score<91) {
+    else if(71<=score && score<81) {
+      System.out.println("C");
+    }
+    else if(81<=score && score<91) {
         System.out.println("B");
-      }
-      else {
+    }
+    else if(91<=score && score<101) {
         System.out.println("A");
-      }
     }
 
     //Problem 4
     int n = 3;
     while(n>2) {
-      System.out.println(n);
-      System.out.println(n+1);
-      System.out.println(n+3);
-      System.out.println(n+5);
-      System.out.println(n+6);
-      n+=12;
+      if(n%3==0 && n%12!=0) {
+        System.out.println(n);
+      }
+      if(n%4==0 && n%12!=0) {
+        System.out.println(n);
+      }
+      n++;
     }
   }
 }
